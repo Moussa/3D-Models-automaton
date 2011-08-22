e023ebbfc7264db16c78b9fc962ef0a0d3ac4af5
Moussekateer <moussa.uk@gmail.com>
2011-08-22 04:54:54 +0100

    updated, still problem with up/down rotations

---
+++ Stitch.py
@@ -0,0 +1,110 @@
+import os, Image, ImageFile
+try:
+	import psyco
+	psyco.full()
+except:
+	pass
+
+targetDimension = 280
+targetSize = 512 * 1024 # 512 KB
+
+def autocrop(img):
+	load = img.load()
+	minX = None
+	minY = img.size[1]
+	maxX = 0
+	maxY = 0
+	for x in xrange(img.size[0]):
+		yTransparent = True
+		for y in xrange(img.size[1]):
+			if load[x, y][3] != 0:
+				if minX is None:
+					minX = x
+				if yTransparent:
+					# Then now is first non-transparent pixel on column y
+					yTransparent = False
+					minY = min(minY, y)
+				maxX = max(maxX, x)
+				maxY = max(maxY, y)
+	cropping = (minX, minY, maxX, maxY)
+	return (img.crop(cropping).copy(), cropping)
+
+def stitch(imagesDir, outpootFile):
+	outpootFile = imagesDir + os.sep + outpootFile
+	print 'Cropping frames...'
+	fullDimensions = (0, 0)
+	maxFrameSize = (None, None)
+	cropped = []
+	croppedNames = {}
+	crops = {}
+	minLeftCrop = None
+	minTopCrop = None
+	minRightCrop = None
+	minBottomCrop = None
+	for i in xrange(24):
+		for s in ('down', '', 'up'):
+			filename = imagesDir + os.sep + str(i) + s + '.png'
+			print 'Processing:', filename
+			img = Image.open(filename).convert('RGBA')
+			maxFrameSize = (max(maxFrameSize[0], img.size[0]), max(maxFrameSize[1], img.size[1]))
+			newI, cropping = autocrop(img)
+			cropped.append(newI)
+			croppedNames[newI] = filename
+			crops[newI] = cropping
+			if minLeftCrop is None or cropping[0] < minLeftCrop:
+				minLeftCrop = cropping[0]
+			if minTopCrop is None or cropping[1] < minTopCrop:
+				minTopCrop = cropping[1]
+			rightCrop = img.size[0] - cropping[2]
+			if minRightCrop is None or rightCrop < minRightCrop:
+				minRightCrop = rightCrop
+			bottomCrop = img.size[1] - cropping[3]
+			if minBottomCrop is None or bottomCrop < minBottomCrop:
+				minBottomCrop = bottomCrop
+			fullDimensions = (fullDimensions[0] + newI.size[0], max(fullDimensions[1], newI.size[1]))
+	print 'Minimum crop size:', (minLeftCrop, minTopCrop, minRightCrop, minBottomCrop)
+	maxFrameSize = (maxFrameSize[0] - minLeftCrop - minRightCrop, maxFrameSize[1] - minTopCrop - minBottomCrop)
+	print 'Max frame size, including cropped area:', maxFrameSize
+	targetRatio = float(targetDimension) / float(max(maxFrameSize))
+	print 'Target scaling ratio:', targetRatio
+	rescaledMaxSize = (int(round(float(maxFrameSize[0]) * targetRatio)), int(round(float(maxFrameSize[1]) * targetRatio)))
+	print 'Computing rescaled sizes, and scaling...'
+	finalSize = (0, 0)
+	resized = []
+	normalizedLeftCrops = {}
+	normalizedTopCrops = {}
+	for f in cropped:
+		newSize = (int(round(float(f.size[0]) * targetRatio)), int(round(float(f.size[1]) * targetRatio)))
+		resImg = f.resize(newSize, Image.ANTIALIAS)
+		resized.append(resImg)
+		normalizedLeftCrops[resImg] = int(round(float(crops[f][0] - minLeftCrop) * targetRatio))
+		normalizedTopCrops[resImg] = int(round(float(crops[f][1] - minTopCrop) * targetRatio))
+		finalSize = (finalSize[0] + newSize[0] + 1, max(finalSize[1], newSize[1] + normalizedTopCrops[resImg]))
+	finalSize = (finalSize[0] - 1, finalSize[1])
+	print 'Rescaling done, building final image.'
+	fullImg = Image.new('RGBA', finalSize, (255, ) * 4)
+	currentOffset = 0
+	offsetMap = []
+	currentOffsetIntervaled = 0
+	for f in resized:
+		fullImg.paste(f, (currentOffsetIntervaled, normalizedTopCrops[f]), f)
+		offsetMap.append(str(currentOffset))
+		offsetMap.append(str(f.size[1]))
+		offsetMap.append(str(normalizedLeftCrops[f]))
+		currentOffset += f.size[0]
+		currentOffsetIntervaled += f.size[0] + 1
+	# Saving
+	print 'Done, saving final image to', outpootFile
+	if os.path.exists(outpootFile):
+		os.remove(outpootFile)
+	ImageFile.MAXBLOCK = finalSize[0] * finalSize[1] * 8 # Make sure there is enough allocated space to save the image as progressive
+	quality = 101
+	while not os.path.exists(outpootFile) or os.stat(outpootFile).st_size > targetSize:
+		quality -= 1
+		fullImg.save(outpootFile, 'JPEG', quality=quality, optimize=True, progressive=True)
+	print 'Saved to', outpootFile, 'with quality', quality
+	print 'Offset map is:', '{{Offset map | ' + str(currentOffset) + ',' + str(rescaledMaxSize[0]) + ',' + str(finalSize[1]) + ',3,' + ','.join(offsetMap) + '}}'
+	h = open(imagesDir + os.sep + 'offsetmap.txt', 'wb')
+	h.write('{{Offset map | ' + str(currentOffset) + ',' + str(rescaledMaxSize[0]) + ',' + str(finalSize[1]) + ',3,' + ','.join(offsetMap) + '}}')
+	h.close()
+	print 'Offset map saved to ' + imagesDir + os.sep + 'offsetmap.txt'
--- automate.py
+++ automate.py
@@ -1,6 +1,7 @@
 import mouse, Image, ImageGrab, os, subprocess, math
 from HLMVModel import *
 from SendKeys import SendKeys
+from Stitch import *
 try:
 	import psyco
 	psyco.full()
@@ -8,77 +9,33 @@ except:
 	pass
 
 degreesToRadiansFactor = math.pi / 180.0
-outputImagesDir = r'output\\' # The directory where the output images will be saved.
-targetImagesDir = r'targetimages\\' # The directory containing the target images for mouse clicking.
+outputImagesDir = r'output' # The directory where the output images will be saved.
+targetImagesDir = r'targetimages' # The directory containing the target images for mouse clicking.
+finalImageName = r'outpoot.jpg'
 monitorResolution = [1920, 1080] # The monitor resolution of the user in the form of a list; [pixel width, pixel height].
-imgCropBoundaries = (280, 42, 1640, 799) # The cropping boundaries, as a pixel distance from the top left corner, for the images as a tuple; (left boundary, top boundary, right boundary, bottom boundary).
+imgCropBoundaries = (1, 42, 1919, 799) # The cropping boundaries, as a pixel distance from the top left corner, for the images as a tuple; (left boundary, top boundary, right boundary, bottom boundary).
 
-def rotateAboutNewCentre(currentXPosition, currentYPosition, lengthOfModel, angle):
+def rotateAboutNewCentre(currentXPosition, currentYPosition, currentZPosition, rotationOffset, yangle, xangle):
 	""" Method to position a model in HLMV with a new center of rotation.
 	
 		Parameters:
                 currentXPosition -> The current x position of the model.
 				currentYPosition -> The current y position of the model.
-				lengthOfModel -> The length of the model (in HLMV units).
-				angle -> The angle the model has been rotated by around the y axis.
+				currentZPosition -> The current z position of the model.
+				rotationOffset -> The distance from the default centre of rotation to the new one (in HLMV units).
+				yangle -> The angle the model has been rotated by around the y axis.
+				xangle -> The angle the model has been rotated by around the x axis.
 	"""
-	angle = float(angle)
-	if angle < 0.0:
-		angle = 360.0 - abs(angle) # Funky HLMV coordinate system
-	newX = (lengthOfModel * math.cos(angle * degreesToRadiansFactor) / 2.0) + float(currentXPosition)
-	newY = (lengthOfModel * math.sin(angle * degreesToRadiansFactor) / 2.0) + float(currentYPosition)
-	return [newX, newY]
-
-"""
-def toAlpha(weights):
-	def getBrightness(p):
-		return int((299.0 * p[0] + 587.0 * p[1] + 114.0 * p[2]) / 1000.0)
-	imgs = {}
-	loaded = {}
-	totalImages = len(weights)
-	totalWeight = 0
-	size = None
-	blackImg = None
-	loadedBlack = None
-	for i in weights:
-		imageobject, imageweight = i
-		imgs[i] = imageobject.convert('RGBA')
-		if size is None:
-			size = imgs[i].size
-		if imageweight:
-			loaded[i] = imgs[i].load()
-			totalWeight += imageweight
-		else:
-			blackImg = i
-			loadedBlack = imgs[i].load()
-	if blackImg is None:
-		return None
-	finalImg = Image.new('RGBA', size)
-	finalLoad = finalImg.load()
-	for x in range(size[0]):
-		for y in range(size[1]):
-			blackPixel = loadedBlack[x, y]
-			totalRed = blackPixel[0]
-			totalGreen = blackPixel[1]
-			totalBlue = blackPixel[2]
-			totalBrightnessDelta = 0
-			initialBrightness = getBrightness(blackPixel)
-			for i in loaded:
-				p = loaded[i][x, y]
-				totalRed += p[0]
-				totalGreen += p[1]
-				totalBlue += p[2]
-				brightness = getBrightness(p)
-				totalBrightnessDelta += brightness - initialBrightness
-			finalLoad[x, y] = (
-				int(totalRed / totalImages),
-				int(totalGreen / totalImages),
-				int(totalBlue / totalImages),
-				int(255 - 255 * totalBrightnessDelta / totalWeight)
-			)
-	return finalImg
-"""
-
+	yangle = float(yangle)
+	xangle = float(xangle)
+	rotationOffset = float(rotationOffset)
+	if yangle < 0.0:
+		yangle = 360.0 - abs(yangle) # Funky HLMV coordinate system
+	newX = (math.cos(yangle * degreesToRadiansFactor) * rotationOffset) + float(currentXPosition)
+	newY = (math.sin(yangle * degreesToRadiansFactor) * rotationOffset) + float(currentYPosition)
+	newZ = float(currentZPosition) - (math.sin(xangle * degreesToRadiansFactor) * rotationOffset)
+	return [newX, newY, newZ]
+	
 def removeBackground(blackBG, whiteBG):
 	""" Method to add transparent background for an image.
 	
@@ -102,73 +59,45 @@ def removeBackground(blackBG, whiteBG):
 				newalpha = int(float(abs(blackpixdata[x, y][0] - whitepixdata[x, y][0]) + abs(blackpixdata[x, y][1] - whitepixdata[x, y][1]) + abs(blackpixdata[x, y][2] - whitepixdata[x, y][2])) / 3.0)
 				blackpixdata[x, y] = (newred, newgreen, newblue, newalpha)
 	return blackBG
-
-"""
-def stitchImages(fileLocation, numberOfImages):
-	imageSize = Image.open(fileLocation + '0.png').size
-	finalImageSize = (imageSize[0] * numberOfImages, imageSize[1])
-	finalImg = Image.new('RGBA', finalImageSize)
-	for imagenumber in range(0, numberOfImages):
-		currentimage = Image.open(fileLocation + str(imagenumber) + '.png')
-		finalImg.paste(currentimage, (imagenumber * imageSize[0], 0))
-	finalImg.save(fileLocation + 'output.png', 'PNG')
-"""
-
-def stitchImages2(fileLocation, numberOfImages):
-	""" Method to stitch together multiple images after they are taken.
 	
-		Parameters:
-                fileLocation -> The location of the files to be stitched.
-				numberOfImages -> Number of images (in one plane) to stitch.
-	"""
-	imageSize = Image.open(fileLocation + '0.png').size
-	finalImageSize = (imageSize[0] * numberOfImages, imageSize[1] * 3)
-	finalImg = Image.new('RGBA', finalImageSize)
-	for imagenumber in range(0, numberOfImages):
-		currentupimage = Image.open(fileLocation + str(imagenumber) + 'up.png')
-		currentimage = Image.open(fileLocation + str(imagenumber) + '.png')
-		currentdownimage = Image.open(fileLocation + str(imagenumber) + 'down.png')
-		finalImg.paste(currentupimage, (imagenumber * imageSize[0], 0))
-		finalImg.paste(currentimage, (imagenumber * imageSize[0], imageSize[1]))
-		finalImg.paste(currentdownimage, (imagenumber * imageSize[0], imageSize[1] * 2))
-	finalImg.save(fileLocation + 'output.png', 'PNG')
-	
-def automateDis(model, numberOfImages=24, modelLength=None, pngcrush=False):
+def automateDis(model, numberOfImages=24, rotationOffset=None):
 	""" Method to automize process of taking images for 3D model views. 
 	
 		Parameters:
                 model -> An instance of a HLMVModelRegistryKey object for the model. Required.
 				numberOfImages -> Number of images to take for one full rotation. Optional, will default to 24.
-				modelLength -> The length of a model. Used in the case of models that do not rotate about the centre of the model. Optional.
-				pngcrush -> Boolean to pngcrush final image. Optional, will default to False.
+				rotationOffset -> The distance from the default centre of rotation to the new one (in HLMV units). Optional.
 	"""
 	n = 0
-	startingpoint = [model.returnTranslation()['x'], model.returnTranslation()['y']]
+	startingpoint = [model.returnTranslation()['x'], model.returnTranslation()['y'], model.returnTranslation()['z']]
+	initialrotation = [model.returnRotation()['x'], model.returnRotation()['y'], model.returnRotation()['z']]
 	mouse.sleep(3)
 	
 	for yrotation in range(-180, 180, 360/numberOfImages):
 		print 'n =', str(n)
-		for xrotation in range (-20, 40, 20):
+		for xrotation in range(-15, 30, 15):
 			# Close HLMV
 			mouse.click(x=monitorResolution[0],y=0)
 			# Set rotation
 			mouse.sleep(2)
-			model.setRotation(x = xrotation, y = yrotation)
+			if initialrotation[1] < 0.0:
+				initialrotation[1] = 360.0 - abs(initialrotation[1])
+			model.setRotation(x = float(initialrotation[0]), y = float(initialrotation[1]) - float(yrotation), z = float(xrotation))
 			print 'rotation =', xrotation, yrotation
-			if modelLength is not None:
-				# Set translation
-				result = rotateAboutNewCentre(startingpoint[0], startingpoint[1], modelLength, yrotation)
+			if rotationOffset is not None:
+				# Set translation to account for off centre rotation
+				result = rotateAboutNewCentre(startingpoint[0], startingpoint[1], startingpoint[2], rotationOffset, yrotation, xrotation)
 				print 'translation =', result
-				model.setTranslation(x = result[0], y = result[1])
+				model.setTranslation(x = result[0], y = result[1], z = result[2])
 			# Set white colour
 			model.setBGColour(255, 255, 255, 255)
 			# Open HLMV
 			mouse.sleep(1)
-			x = mouse.find({targetImagesDir + 'openhlmv.png': (0, 0)})
+			x = mouse.find({targetImagesDir + os.sep + 'openhlmv.png': (0, 0)})
 			if x is None:
-				x = mouse.find({targetImagesDir + 'openhlmvunhighlighted.png': (0, 0)})
+				x = mouse.find({targetImagesDir + os.sep + 'openhlmvunhighlighted.png': (0, 0)})
 			if x is None:
-				x = mouse.find({targetImagesDir + 'openhlmvinactive.png': (0, 0)})
+				x = mouse.find({targetImagesDir + os.sep + 'openhlmvinactive.png': (0, 0)})
 			if x is None:
 				print 'Couldn\'t find source SDK launcher to click on'
 				break
@@ -177,14 +106,14 @@ def automateDis(model, numberOfImages=24, modelLength=None, pngcrush=False):
 			mouse.sleep(2)
 			SendKeys("""*{UP}""")
 			# Open recent model
-			mouse.find({targetImagesDir + 'filemenubutton.png': (0, 0)}, clickpoint=True)
+			mouse.find({targetImagesDir + os.sep + 'filemenubutton.png': (0, 0)}, clickpoint=True)
 			SendKeys("""{DOWN 8}{RIGHT}{ENTER}""")
 			# Take whiteBG screenshot and crop
 			mouse.sleep(2)
 			imgWhiteBG = ImageGrab.grab()
 			imgWhiteBG = imgWhiteBG.crop(imgCropBoundaries)
 			# Change BG colour to black
-			mouse.find({targetImagesDir + 'optionsmenubutton.png': (0, 0)}, clickpoint=True)
+			mouse.find({targetImagesDir + os.sep + 'optionsmenubutton.png': (0, 0)}, clickpoint=True)
 			SendKeys("""{DOWN}{ENTER}""")
 			mouse.sleep(1)
 			SendKeys("""{LEFT 7}{SPACE}{ENTER}""")
@@ -195,31 +124,22 @@ def automateDis(model, numberOfImages=24, modelLength=None, pngcrush=False):
 			# Remove background from image
 			img = removeBackground(imgBlackBG, imgWhiteBG)
 			# Save screenshot
-			if xrotation == -20:
+			if xrotation == -15:
 				imgname = str(n) + 'up.png'
-			elif xrotation == 20:
+			elif xrotation == 15:
 				imgname = str(n) + 'down.png'
 			else:
 				imgname = str(n) + '.png'
-			img.save(outputImagesDir + imgname, "PNG")
+			img.save(outputImagesDir + os.sep + imgname, "PNG")
 		n += 1
 	# Close HLMV finally
 	mouse.click(x = monitorResolution[0], y = 0)
 	# Stitch images together
 	print 'Stitching images together...'
-	stitchImages2(outputImagesDir, numberOfImages)
-	# Crush images if specified
-	if pngcrush:
-		files = os.listdir(outputImagesDir)
-		files.sort()
-		for file in files:
-			print "Processing " + file + "..."
-			print outputImagesDir
-			outpoot = file[:-4] + r'compressed.png'
-			subprocess.call(['pngcrush', '-rem', 'gAMA', '-rem', 'cHRM', '-rem', 'iCCP', '-rem', 'sRGB', '-brute', outputImagesDir + file, outputImagesDir + outpoot])
+	stitch(outputImagesDir, finalImageName)
 	# All done yay
 	print '\nAll done'
 
 # Example Usage for Flare Gun
-model = HLMVModelRegistryKey('models.weapons.c_models.c_flaregun_pyro.c_flaregun_pyro.mdl')
-automateDis(model=model, numberOfImages=24, modelLength=19.0)
+model = HLMVModelRegistryKey('models.weapons.c_models.c_pocket_watch.parts.c_pocket_watch.mdl')
+automateDis(model=model)
