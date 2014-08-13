b943ebe07f0c92eefd9d2c1c23de1c0d67fb67e0
Moussekateer <moussa.uk@gmail.com>
2014-08-13 04:05:22 +0100

    update file uploader

--- uploadFile.py
+++ uploadFile.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python -OO
 # -*- coding: utf-8 -*-
-import os, wikiUpload, urllib2, hashlib
+import os, wikitools, urllib2, hashlib
 
 def fileURL(filename, siteaddress=r'http://wiki.teamfortress.com/w/images/'):
 	filenamehash = hashlib.md5(filename.replace(' ', '_')).hexdigest()
 	return siteaddress + filenamehash[:1] + '/' + filenamehash[:2] + '/' + filename.replace(' ', '_')	
 
-def uploadFile(file, title, description, username, password, category='', wikiURL='http://wiki.tf2.com/w/', license='', fileprefix='', filesuffix='', overwrite=False):
+def uploadFile(file, title, description, username, password, category='', wikiURL='http://wiki.tf2.com/w/api.php', license='', fileprefix='', filesuffix='', overwrite=False):
 	if username is None or password is None:
 		return
-	uploader = wikiUpload.wikiUploader(username, password, wikiURL)
+	wiki = wikitools.wiki.Wiki(wikiURL)
+	wiki.login(username, password)
+	uploader = wikitools.wikifile.File(wiki=wiki, title=u'File:' + title)
 	try:
 		print 'Uploading', file, 'as', title, '...'
 		try:
-			uploader.upload(file, title, description + category, license, overwrite=overwrite)
+			uploader.upload(fileobj=open(file, "rb"), ignorewarnings=True, comment=description)
+			wikitools.page.Page(wiki, u'File:' + title).edit(description + category, summary=u'', minor=True, bot=False, skipmd5=True)
 		except:
 			print 'Failed', file
 			return None
